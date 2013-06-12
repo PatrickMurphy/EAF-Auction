c6a560890fb83bcb820aca0aaa01fbf1c98f1286
Patrick Murphy <patrick@patrickmurphyphoto.com>
2013-06-11 22:14:06 -0700

    Finishing touches
    Did some finishing touches to the homepage

---
+++ index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html>	
+
+<head>
+
+	<title>Alaska Airlines Employee Assistance Fund</title>
+
+	<script src="jquery-1.10.0.min.js"></script>
+
+	<script src="javascript.js" type="text/javascript" ></script>
+
+	<link href="stylesheet.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body onload="initSlideShow()">
+
+	<div id="main" class="centered">
+
+		<div id="banner">
+
+			<div id="bannerLogo"></div>
+
+			<div id="bannerText">Alaska Airlines<br/>Employee<br/>Assistance<br/>Fund</div>
+
+			<div class="searchButton">Search</div>
+
+			<input class="searchBar" type="text" />
+
+		</div>
+
+		
+
+		<div id="navigation">
+
+			<div class="navItem"><a class="navItemLink" href="index.html">Home</a></div>
+
+			<div class="navItem"><a class="navItemLink" href="index.html">Browse</a></div>
+
+			<div class="navItem"><a class="navItemLink" href="index.html">News</a></div>
+
+			<a class="navItem navLeftCap"></a>
+
+			
+
+			<div class="navItem right" ><a class="navItemLink" href="index.html">Register</a></div>
+
+			<div class="navItem right" ><a class="navItemLink" href="index.html">Login</a></div>
+
+			<a class="navItem navRightCap" ></a>
+
+		</div>
+
+		
+
+		<div id="page" >
+
+			<div id="hotItemGallery">
+
+				<div class="hotItemImage" id="url(hotdeal_1.png)"></div>
+
+				<div class="hotItemImage" id="url(hotdeal_3.png)"></div>
+
+				<div class="hotItemImage" id="url(hotdeal_2.png)"></div>
+
+				<div class="hotItemImage" id="url(hotdeal_4.png)"></div>
+
+				<div class="hotItemImage" id="url(hotdeal_5.png)"></div>
+
+				<div class="hotItemImage" id="url(hotdeal_6.png)"></div>
+
+				
+
+				<div class="hotItemBlips">
+
+					<div onclick="manualHotItemNav(0)" class="hotItemBlip"></div>
+
+					<div onclick="manualHotItemNav(1)" class="hotItemBlip"></div>
+
+					<div onclick="manualHotItemNav(2)" class="hotItemBlip"></div>
+
+					<div onclick="manualHotItemNav(3)" class="hotItemBlip"></div>
+
+					<div onclick="manualHotItemNav(4)" class="hotItemBlip"></div>
+
+					<div onclick="manualHotItemNav(5)" class="hotItemBlip"></div>
+
+				</div>
+
+			</div>
+
+			
+
+			<div class="hotItemDescGallery">
+
+				<div class="hotItemDesc">
+
+					<div class="hotItemTitle">Full Fare - Round Trip Ticket</div>
+
+					<div class="hotItemDetails">Alaska Arilines and/or Horizon Air FULL fair, UNRESTRICTED ticket, NO Blackout dates applied.<br/><br/>Value: $1,250<br/><br/>Donated by: Alaska Airlines</div>
+
+					<div class="hotItemTime" endtime="<?php print date("n/j/Y g:i A",time()-(9*60*60)+(60*10)); ?>"></div>
+
+					<div class="bidNow" >Bid Now</div>
+
+				</div>
+
+				<div class="hotItemDesc">
+
+					<div class="hotItemTitle" >Full Fare - Round Trip Ticket</div>
+
+					<div class="hotItemDetails" >Alaska Arilines and/or Horizon Air FULL fair, UNRESTRICTED ticket, NO Blackout dates applied.<br/><br/>Value: $1,500<br/><br/>Donated by: Alaska Airlines</div>
+                    
+					<div class="hotItemTime" endtime="<?php print date("n/j/Y g:i A",time()-(9*60*60)+(60*9)); ?>"></div>
+
+					<div class="bidNow" >Bid Now</div>
+
+				</div>
+
+				<div class="hotItemDesc">
+
+					<div class="hotItemTitle" >Full Fare - Round Trip Ticket</div>
+
+					<div class="hotItemDetails" >Alaska Arilines and/or Horizon Air FULL fair, UNRESTRICTED ticket, NO Blackout dates applied.<br/><br/>Value: $1,000<br/><br/>Donated by: Alaska Airlines</div>
+
+					<div class="hotItemTime" endtime="<?php print date("n/j/Y g:i A",time()-(9*60*60)+(60*25)); ?>"></div>
+
+					<div class="bidNow" >Bid Now</div>
+
+				</div>
+
+				<div class="hotItemDesc">
+
+					<div class="hotItemTitle" >Full Fare - Round Trip Ticket</div>
+
+					<div class="hotItemDetails" >Alaska Arilines and/or Horizon Air FULL fair, UNRESTRICTED ticket, NO Blackout dates applied.<br/><br/>Value: $1,300<br/><br/>Donated by: Alaska Airlines</div>
+
+					<div class="hotItemTime" endtime="<?php print date("n/j/Y g:i A",time()-(9*60*60)+(60*100)); ?>"></div>
+
+					<div class="bidNow" >Bid Now</div>
+
+				</div>
+
+				<div class="hotItemDesc">
+
+					<div class="hotItemTitle" >Full Fare - Round Trip Ticket</div>
+
+					<div class="hotItemDetails" >Alaska Arilines and/or Horizon Air FULL fair, UNRESTRICTED ticket, NO Blackout dates applied.<br/><br/>Value: $1,189<br/><br/>Donated by: Alaska Airlines</div>
+
+					<div class="hotItemTime" endtime="<?php print date("n/j/Y g:i A",time()-(9*60*60)+(60*20)); ?>"></div>
+
+					<div class="bidNow" >Bid Now</div>
+
+				</div>
+
+				<div class="hotItemDesc">
+
+					<div class="hotItemTitle" >Full Fare - Round Trip Ticket</div>
+
+					<div class="hotItemDetails" >Alaska Arilines and/or Horizon Air FULL fair, UNRESTRICTED ticket, NO Blackout dates applied.<br/><br/>Value: $987<br/><br/>Donated by: Alaska Airlines</div>
+
+					<div class="hotItemTime" endtime="<?php print date("n/j/Y g:i A",time()-(9*60*60)+(60*9)); ?>"></div>
+
+					<div class="bidNow" >Bid Now</div>
+
+				</div>
+
+				
+
+			</div>
+
+			<br style="clear:both;" />
+			<div class="box580 left">
+				<div class="header" style="width:100%;">Categories</div>
+            	<ul class="twocol categoryListHome">
+            		<li><a href="#">Alaska</a></li>
+                    <li><a href="#">California</a></li>
+                    <li><a href="#">Dining Out</a></li>
+                    <li><a href="#">Electronics</a></li>
+                    <li><a href="#">Food and Wine</a></li>
+                    <li><a href="#">Handmade</a></li>
+                    <li><a href="#">Home and Garden</a></li>
+                    <li><a href="#">Mexico</a></li>
+                    <li><a href="#">Personal Goods</a></li>
+                    <li><a href="#">Portland Area</a></li>
+                    <li><a href="#">Software & Games</a></li>
+                    <li><a href="#">Vacations</a></li>
+                    <li><a href="#">Art</a></li>
+                    <li><a href="#">Clothing</a></li>
+                    <li><a href="#">Eastern Washington</a></li>
+                    <li><a href="#">Entertainment</a></li>
+                    <li><a href="#">Gift Baskets</a></li>
+                    <li><a href="#">Hawaii</a></li>
+                    <li><a href="#">Jewelry</a></li>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">Personal Services</a></li>
+                    <li><a href="#">Seattle Area</a></li>
+                    <li><a href="#">Sports Equipment & Events</a></li>
+            	</ul>
+            </div>
+
+			<div class="left box280">
+				<div class="header" style="width:100%;">News</div>
+                <h1>We made $110,000 This Year!</h1>
+                <p>Thank you all so much for making this a great year in spite of the difficulties. We are working on sending the email/invoices out - be patient, there's a lot of them. <a href="#" class="right">Read More</a></p>
+                <h1>Personal Thank You for 13 Years</h1>
+                <p>This is my last year working for the EAF on this auction.  I want to offer my sincere thanks to all of our bidders, our donors, the employees of Alaska Airlines, the Board and staff of the EAF, and the...  <a href="#" class="right">Read More</a></p>
+                <h1>Pickup and Payment Info</h1>
+                <p>This year’s auction made over $110,000!  Another amazing year.  Thank you all so much.  We didn’t hit our $1M mark, so I expect you all to come back and bid some... <a href="#" class="right">Read More</a></p>
+             </div>
+
+		</div>
+
+		
+
+		<div id="bottomBar"> © Alaska Airlines Employee Assistance Fund 2013. All Rights Reserved.
+
+			<span class="bottomBarNav"><a href="">News</a> | <a href="">Browse</a> | <a href="">Home</a></span>
+
+		</div>
+
+	</div>
+
+</body>
+
+</html>
