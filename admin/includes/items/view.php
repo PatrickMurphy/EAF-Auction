5635e5f206192900354cc05353eb1ef98a9ac825
Patrick Murphy <patrick@patrickmurphyphoto.com>
2013-08-15 14:25:45 -0700

    View Items

---
+++ admin/includes/items/view.php
@@ -0,0 +1,32 @@
+<table>
+ <th>
+	<td>ID#</td>
+	<td>Title</td>
+	<td>Start Date</td>
+	<td>End Date</td>
+	<td>Value</td>
+	<td>Current Bid</td>
+	<td>Shipple</td>
+	<td>Used</td>
+	<td>Actions</td>
+ </th>
+ <tr>
+	<td>001</td>
+	<td>Round trip ticket to Hawaii</td>
+	<td>10/22/13 8:00am</td>
+	<td>11/10/13 8:00am</td>
+	<td>$600.00</td>
+	<td>$0.00</td>
+	<td>n/a</td>
+	<td>no</td>
+	<td>
+		<select>
+			<option>Edit</option>
+			<option>Delete</option>
+			<option>Hot Item</option>
+			<option>Favorite</option>
+		</select>
+		<button>Go</button>
+	</td>
+ </tr>
+</table>
